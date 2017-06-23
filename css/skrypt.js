73bdc3d1775035cf824e1ed3479e95ef01d7a774
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-23 22:20:57 +0200

    cos tam smiga

--- css/skrypt.js
+++ css/skrypt.js
@@ -1,11 +1,41 @@
 'use strict';
 
-function calc(){
-    console.log('click');
-    document.getElementById("wyswietlacz").innerHTML = 7;
-}
+function calc(wartosc) {
 
-document.getElementById("siedem").onclick = calc;
+    switch (wartosc) {
+        case 1:
+            wyswietlacz.innerHTML = wyswietlacz.innerHTML + jeden.innerHTML;
+            break;
+        case 2:
+            wyswietlacz.innerHTML = wyswietlacz.innerHTML + dwa.innerHTML;
+            break;
+        case 3:
+            wyswietlacz.innerHTML = wyswietlacz.innerHTML + trzy.innerHTML;
+            break;
+        case 4:
+            wyswietlacz.innerHTML = wyswietlacz.innerHTML + cztery.innerHTML;
+            break;
+        case 5:
+            wyswietlacz.innerHTML = wyswietlacz.innerHTML + piec.innerHTML;
+            break;
+        case 6:
+            wyswietlacz.innerHTML = wyswietlacz.innerHTML + szesc.innerHTML;
+            break;
+        case 7:
+            wyswietlacz.innerHTML = wyswietlacz.innerHTML + siedem.innerHTML;
+            break;
+        case 8:
+            wyswietlacz.innerHTML = wyswietlacz.innerHTML + osiem.innerHTML;
+            break;
+        case 9:
+            wyswietlacz.innerHTML = wyswietlacz.innerHTML + dziewiec.innerHTML;
+            break;
+        case 0:
+            wyswietlacz.innerHTML = wyswietlacz.innerHTML + zero.innerHTML;
+            break;
+
+    }
+}
 
 var wyswietlacz = document.getElementById('wyswietlacz');
 var zero = document.getElementById('zero');
@@ -25,9 +55,33 @@ var dodawanie = document.getElementById('dodawanie');
 var odejmowanie = document.getElementById('odejmowanie');
 var suma = document.getElementById('suma');
 
-
-
-
-document.getElementById("zero").addEventListener("click", function(){
-    document.getElementById("wyswietlacz").innerHTML = 0;
-})
+jeden.addEventListener('click', function (e) {
+    calc(1);
+});
+dwa.addEventListener('click', function (e) {
+    calc(2);
+});
+trzy.addEventListener('click', function (e) {
+    calc(3);
+});
+cztery.addEventListener('click', function (e) {
+    calc(4);
+});
+piec.addEventListener('click', function (e) {
+    calc(5);
+});
+szesc.addEventListener('click', function (e) {
+    calc(6);
+});
+siedem.addEventListener('click', function (e) {
+    calc(7);
+});
+osiem.addEventListener('click', function (e) {
+    calc(8);
+});
+dziewiec.addEventListener('click', function (e) {
+    calc(9);
+});
+zero.addEventListener('click', function (e) {
+    calc(0);
+});
